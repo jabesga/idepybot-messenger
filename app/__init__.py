3998072a70dfc28f9509e963d604f4b2f17f9109
Jon Ander Besga <jonan.bsg@gmail.com>
2016-07-27 17:24:11 +0200

    adding answer to message

--- app/__init__.py
+++ app/__init__.py
@@ -19,15 +19,26 @@ payload = {
 response = requests.post(url, json=payload)
 print(response.content)
 
+def send_message(user_id, message):
+	url = "https://graph.facebook.com/v2.6/me/thread_settings?access_token={0}".format(PAGE_ACCESS_TOKEN)
+	payload = {
+		'recipient': user_id
+		'message': message
+	}
+
+	json_response = request.post(url, data=payload)
+	return json_response
+
 @app.route("/webhook", methods=['GET','POST'])
 def hook():
 	if request.method == 'POST':
-		print(request.get_json())
-		
+		response = request.get_json()
+		print(response)
+		json_response = send_message(response['entry'][0]['messaging'][0]['recipient']['id'], 'This is a robot')
+		print(json_response)
+		return '200'
 	else:
 		if request.args.get('hub.verify_token') == VERIFY_TOKEN:
 			return request.args.get('hub.challenge')
 		else:
 			return 'Error, wrong validation token'
-
-	
