09ca9f21d1516601ba4e5bc8ba4028ee305d6c52
Jon Ander Besga <jonan.bsg@gmail.com>
2016-07-27 17:46:27 +0200

    code structured

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from bot import Bot
 import requests
 import os
 
@@ -8,38 +9,20 @@ app.debug = True
 VERIFY_TOKEN = os.environ['verify_token']
 PAGE_ACCESS_TOKEN = os.environ['page_access_token']
 
-url = "https://graph.facebook.com/v2.6/me/thread_settings?access_token={0}".format(PAGE_ACCESS_TOKEN)
-payload = {
-	'setting_type': 'greeting',
-	'greeting': {
-		'text': 'Welcome to the new testing bot!'
-	}
-}
 
-response = requests.post(url, json=payload)
-print(response.content)
+bot = Bot(PAGE_ACCESS_TOKEN)
+bot.change_greeting('Hello, this is a new greeting')
 
-def send_message(user_id, message):
-	url = "https://graph.facebook.com/v2.6/me/messages?access_token={0}".format(PAGE_ACCESS_TOKEN)
-	print(user_id)
-	payload = {
-		'recipient': {'id': user_id},
-		'message': {'text':message}
-	}
-
-	json_response = requests.post(url, json=payload)
-	return json_response
-
-@app.route("/webhook", methods=['GET','POST'])
-def hook():
-	if request.method == 'POST':
-		response = request.get_json()
-		print(response)
-		json_response = send_message(response['entry'][0]['messaging'][0]['sender']['id'], 'This is a robot')
-		print(json_response.content)
-		return '200'
+@app.route("/webhook", methods=['GET'])
+def verify():
+	if request.args.get('hub.verify_token') == VERIFY_TOKEN:
+		return request.args.get('hub.challenge')
 	else:
-		if request.args.get('hub.verify_token') == VERIFY_TOKEN:
-			return request.args.get('hub.challenge')
-		else:
-			return 'Error, wrong validation token'
+		return 'Error, wrong validation token'
+
+@app.route("/webhook", methods=['POST'])
+def webhook():
+	response = request.get_json()
+	json_response = bot.send_message(response['entry'][0]['messaging'][0]['sender']['id'], 'This is a robot')
+	return '200'
+
