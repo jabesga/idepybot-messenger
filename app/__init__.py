5b8f66ebd9d9a3f2548915e71ea26b2d4a7d991c
Jon Ander Besga <jonan.bsg@gmail.com>
2016-07-27 18:08:04 +0200

    deleting echo

--- app/__init__.py
+++ app/__init__.py
@@ -24,8 +24,8 @@ def verify():
 def webhook():
 	response = request.get_json()
 	print(response)
-	if not response['entry'][0]['messaging'][0]['is_echo']:
-		json_response = bot.send_message(response['entry'][0]['messaging'][0]['sender']['id'], 'This is a robot')
-		print('RESPONDIDO {0}'.format(json_response))
+	
+	json_response = bot.send_message(response['entry'][0]['messaging'][0]['sender']['id'], 'This is a robot')
+	print('RESPONDIDO {0}'.format(json_response))
 	return '200'
 
